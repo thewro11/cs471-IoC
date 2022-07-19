1e3c5574a473e7795a8eba129626987869fa3e29
Théo Teenster <thewro11@gmail.com>
2022-07-19 20:14:23 +0700

    implement Spring as annotation-based

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,12 +1,18 @@
 package atm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        DataSourceDB dataSourceDB = new DataSourceDB();
-        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
-        Bank bank = new Bank("My Bank", dataSourceFile);
-        ATM atm = new ATM(bank);
-        AtmUI atmUI = new AtmUI(atm);
+        // Dependency Injection by SPRING
+        ApplicationContext app = new ClassPathXmlApplicationContext("bean.xml");
+        AtmUI atmUI = app.getBean(AtmUI.class);
         atmUI.run();
+
+        // Similar to
+        // new AnnotationConfigApplicationContext(AtmConfig.class).getBean(AtmUI.class).run();
+        // Using SPRING makes code shorter.
     }
 }
